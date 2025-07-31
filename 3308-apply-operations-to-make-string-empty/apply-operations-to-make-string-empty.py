137ccc0f15ed5a70b340bd2ffae4e847f6ee2870
gramzzzzzz <108583148+gramzzzzzz@users.noreply.github.com>
2025-07-31 08:02:02 +0530

    Time: 659 ms (14.38%) | Memory: 20.5 MB (57.50%) - LeetSync

---
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lastNonEmptyString(self, s: str) -> str:
+        n = len(s)
+        d = Counter(s)
+        # print(d)
+        t = max(d.values())
+        # print(t)
+        ch = set()
+        for i in d:
+            if d[i] == t:
+                ch.add(i)
+        # print(ch)
+        ans = ""
+        for i in s:
+            if d[i] == 1 and i in ch:
+                ans += i
+            d[i] -= 1
+        return ans
