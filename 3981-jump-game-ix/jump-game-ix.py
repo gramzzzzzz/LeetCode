5a34589544b2d423ba0e45557eee23cef775eb31
Shriram Govind <108583148+gramzzzzzz@users.noreply.github.com>
2025-08-24 20:11:42 +0530

    Time: 1397 ms (0.00%) | Memory: 43.5 MB (0.00%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.py
+++ 3981-jump-game-ix/jump-game-ix.py
@@ -13,15 +13,10 @@ class Solution:
         prefm[0] = nums[0]
         for i in range(1, n):
             prefm[i] = max(prefm[i-1], nums[i])
-        # print(arr)
-        # print(pref)
-        # print(prefm)
         ans = [0] * n
         for i in range(n-1, -1, -1):
             m = prefm[i]
             ind = bisect_right(arr, (m-1, float('inf')))
-            # print(nums[i], m, ind)
-            # ind = min(ind, n-1)
             if ind > 0:
                 target_ind = pref[ind-1]
                 ans[i] = max(prefm[target_ind], ans[target_ind], nums[i])
