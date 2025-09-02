9dd0b33745aa9536d4805260bc115f61ba6f9b55
Shriram Govind <108583148+gramzzzzzz@users.noreply.github.com>
2025-09-02 13:35:40 +0530

    Time: 37 ms (61.54%) | Memory: 17.9 MB (20.51%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
@@ -3,12 +3,18 @@ class Solution:
         n = len(points)
         points.sort(key=lambda x: (x[0], -x[1]))
         ans = 0
+        # print(points)
         for i in range(n):
+            miny = float('inf')
             for j in range(i-1, -1, -1):
-                if points[i][1] <= points[j][1]:
+                if points[j][1] < miny and points[j][1] >= points[i][1]:
                     ans += 1
-                    for k in range(j+1, i):
-                        if points[i][1] <= points[k][1] <= points[j][1]:
-                            ans -= 1
-                            break
+                if points[j][1] >= points[i][1]:
+                    miny = min(miny, points[j][1])
+                # if points[i][1] <= points[j][1]:
+                #     ans += 1
+                #     for k in range(j+1, i):
+                #         if points[i][1] <= points[k][1] <= points[j][1]:
+                #             ans -= 1
+                #             break
         return ans
